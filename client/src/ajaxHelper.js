3981f301109a1da895852bdf39cfd74bc5843591
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-12-13 18:20:20 +0000

    adding ajax helper

---
+++ client/src/ajaxHelper.js
@@ -0,0 +1,15 @@
+var makeGetRequest = function( url, callback ) {
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.open( 'GET', url );
+    httpRequest.onload = function() {
+      if ( this.status == 200 ) {
+        callback( JSON.parse( this.responseText ) );
+      } else {
+        console.log( "Request failed with status ", this.status );
+      }
+    };
+    httpRequest.send();
+  }
+};
+
+module.exports = makeGetRequest;
